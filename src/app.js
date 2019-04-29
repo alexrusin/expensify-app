e1263d93fb8f094315d06881b45137a2f6b3429f
Alex Rusin <alex@sunriseintegration.com>
2019-04-29 14:10:10 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+    <div>This is from my Dashboard component</div>
+);
+
+const AddExpensePage = () => (
+    <div>This is from my add expense page</div>
+);
+
+const EditExpensePage = () => (
+    <div>This is from my edit expense page</div>
+);
+
+const HelpPage = () => (
+    <div>This is from my help page</div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+        
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
