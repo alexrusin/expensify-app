3de4599e578d4e1c8afe2bef35536f1e42fff1c6
Alex Rusin <alex@sunriseintegration.com>
2019-05-28 08:50:10 -0700

    moment and date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
 
     onDescriptionChange = (e) => {
@@ -22,6 +28,14 @@ class ExpenseForm extends React.Component {
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
+    };
+
+    onDateChange = (createdAt) => {
+        this.setState(() =>({createdAt}));
+    };
+
+    onFocusChange = ({focused}) => {
+        this.setState(() => ({calendarFocused: focused}));
     };
 
     render() {
@@ -47,6 +61,14 @@ class ExpenseForm extends React.Component {
                         onChange={this.onNoteChange}
                     >                    
                     </textarea>
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
                     <button>Add Expense</button>
                 </form>
             </div>
